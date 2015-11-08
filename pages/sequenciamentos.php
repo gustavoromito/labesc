288c00360490996dd4879063e7815b4ca4e65629
Gustavo Nogueira <gustavo.nogueira@mowa.com.br>
2015-11-08 16:13:53 -0200

    some changes

--- pages/sequenciamentos.php
+++ pages/sequenciamentos.php
@@ -3,7 +3,21 @@
 
 <?php include("../php/head.php"); ?>
 <?php include("../php/databaseManager.php"); ?>
+<script>
+    $(document).ready(function() {
+        $("#cancelBtn").click(function (){
+            console.log("ENTROU");
+            $("#list-sequenciamento").show();
+            $("#new-sequenciamento").hide();
+        });
 
+        $("#newBtn").click(function (){
+            console.log("AQUI");
+            $("#list-sequenciamento").hide();
+            $("#new-sequenciamento").show();
+        });
+    });
+</script>
 <body>
 
     <div id="wrapper">
@@ -11,60 +25,131 @@
         <?php include("../php/navigation.php"); ?>
 
         <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Sequenciamentos</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Lista de todos Sequenciamentos do seu acesso
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="dataTable_wrapper">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th>Data</th>
-                                            <th>Descrição</th>
-                                            <th>Pesquisador</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr class="even gradeC">
-                                            <td>12/12/1994</td>
-                                            <td>Sequenciamento Sapo</td>
-                                            <td>Mateus Loren</td>
-                                        </tr>
-                                        <tr class="odd gradeA">
-                                            <td>12/12/1994</td>
-                                            <td>Sequenciamento Sapo</td>
-                                            <td>Gustavo Romito</td>
-                                        </tr>
-                                        <tr class="even gradeA">
-                                            <td>12/12/1994</td>
-                                            <td>Sequenciamento Sapo</td>
-                                            <td>Leonardo Paladia</td>
-                                        </tr>
-
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <!-- /.panel-body -->
+            <div id="list-sequenciamento">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header" style="float: left; width: 80%;">Sequenciamentos</h1>
+                        <button type="button" style="margin: 40px 0 20px; width: 15%; float: right;" class="btn btn-outline btn-primary" id="newBtn">Novo Sequenciamento</button>
                     </div>
-                    <!-- /.panel -->
+                    <!-- /.col-lg-12 -->
                 </div>
-                <!-- /.col-lg-12 -->
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Lista de todos Sequenciamentos do seu acesso
+                            </div>
+                            <!-- /.panel-heading -->
+                            <div class="panel-body">
+                                <div class="dataTable_wrapper">
+                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                        <thead>
+                                            <tr>
+                                                <th>Data</th>
+                                                <th>Descrição</th>
+                                                <th>Pesquisador</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="even gradeC">
+                                                <td>12/12/1994</td>
+                                                <td>Sequenciamento Sapo</td>
+                                                <td>Mateus Loren</td>
+                                            </tr>
+                                            <tr class="odd gradeA">
+                                                <td>12/12/1994</td>
+                                                <td>Sequenciamento Sapo</td>
+                                                <td>Gustavo Romito</td>
+                                            </tr>
+                                            <tr class="even gradeA">
+                                                <td>12/12/1994</td>
+                                                <td>Sequenciamento Sapo</td>
+                                                <td>Leonardo Paladia</td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <!-- /.panel-body -->
+                        </div>
+                        <!-- /.panel -->
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
             </div>
+
+            <div id="new-sequenciamento" style="display: none;">
+                <div class="row">
+                    <button type="button" class="btn btn-outline btn-danger" id="cancelBtn">Cancelar</button>
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Novo Sequenciamento</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <form role="form">
+                                    <div class="form-group">
+                                        <label>Tipo Sequenciamento</label>
+                                        <div class="radio" style="display: inline-block;">
+                                            <label>
+                                                <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">Único
+                                            </label>
+                                        </div>
+                                        <div class="radio" style="display: inline-block;">
+                                            <label>
+                                                <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Múltiplo
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Descrição</label>
+                                        <input class="form-control" placeholder="Exemplo: Sequenciamento Sapo na Lagoa">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Pesquisador</label>
+                                        <select class="form-control">
+                                            <option>Pesquisador 1</option>
+                                            <option>Pesquisador 2</option>
+                                            <option>Pesquisador 3</option>
+                                            <option>Pesquisador 4</option>
+                                            <option>Pesquisador 5</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Observações</label>
+                                        <textarea class="form-control" rows="3"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Eletroferogramas</label>
+                                        <input type="file">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Nucleotídicas</label>
+                                        <input type="file">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Mapa da Placa</label>
+                                        <input type="file">
+                                    </div>
+                                    <button type="submit" class="btn btn-default">Enviar</button>
+                                    <button type="reset" class="btn btn-default">Cancelar</button>
+                                </form>
+                            </div>
+                            <!-- /.panel-body -->
+                        </div>
+                        <!-- /.panel -->
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+            <div>
         </div>
         <!-- /#page-wrapper -->
-
     </div>
     <!-- /#wrapper -->
 
